4c62da593a782e4c9f23b045d0bc64b19ebdee4b
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-08-19 23:50:13 +0700

    modified home component

--- src/components/home.js
+++ src/components/home.js
@@ -1,13 +1,15 @@
 import Row from "./row"
 import IntroText from "./intro-text"
 import IntroImage from "./intro-image"
+import Button from "./button"
+import HomeImg from '../images/HomeBg.png'
 
 
 export default function Home (props) {
   return (
     <div className="home">
       <Row className="home_f-row">
-        
+
       </Row>
     </div>
   )
