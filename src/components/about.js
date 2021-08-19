d3c5f39a3900e9c2103bd96f4c1f53e58c341c46
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-08-19 23:49:18 +0700

    .

--- src/components/about.js
+++ src/components/about.js
@@ -1,13 +1,33 @@
 import Row from "./row"
 import IntroText from "./intro-text"
 import IntroImage from "./intro-image"
-
+import Button from "./button"
 
 export default function About (props) {
     return (
       <div className="about">
         <Row className="about__f-row">
-          
+          <IntroText>
+            <p>
+            We provide easy solution 
+            to exchange your <span>
+            Bitcoin/
+            GiftCard for money
+            </span>
+            </p>
+
+            <p className="intro-text--light">
+            CoinBase is a platform for trading your bitcoin and 
+            giftcard at the best rate, why not give us a trial.
+            </p>
+
+          <Button
+            name="get started" className="button--white-linear" 
+            textClassName="button__text--linear"
+          />
+      </IntroText>
+
+      <IntroImage image={HomeImg} />
         </Row>
       </div>
     )
