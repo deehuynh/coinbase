e7e72f92bccaa2b3b57ced85c37687c8e9ff9743
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-08-20 22:17:21 +0700

    modified about component

--- src/components/about.js
+++ src/components/about.js
@@ -9,25 +9,7 @@ export default function About (props) {
     return (
       <div className="about">
         <Row className="about__f-row">
-          <IntroText>
-            <p>
-            We provide easy solution 
-            to exchange your <span>
-            Bitcoin/
-            GiftCard for money
-            </span>
-            </p>
-
-            <p className="intro-text--light">
-            CoinBase is a platform for trading your bitcoin and 
-            giftcard at the best rate, why not give us a trial.
-            </p>
-
-            <Button
-              name="get started" className="button--white-linear" 
-              textClassName="button__text--linear"
-            />
-          </IntroText>
+          
         </Row>
       </div>
     )
