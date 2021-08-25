eff4656c64eed4768855574354a30fbfe0fb3198
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-08-26 06:06:40 +0700

    .

--- src/components/about.js
+++ src/components/about.js
@@ -2,6 +2,7 @@
 import Row from "./row"
 //Import column
 import Col from './column'
+import ColImg from './column-image'
 // Import images
 import IMG1 from '../images/about-img-1.png'
 
@@ -23,9 +24,7 @@ export default function About (props) {
             </p>
           </Col>
 
-          <Col className="about__col-1-2">
-
-          </Col>
+          <ColImg className="about__col-1-2" image={IMG1} />
         </Row>
       </div>
     )
