7c613e107bf2cc8c485fa725e5a31233e3a0c90f
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-08-25 19:24:47 +0700

    .

--- src/components/about.js
+++ src/components/about.js
@@ -8,7 +8,9 @@ export default function About (props) {
       <div className="about">
         <Row className="about__1st-row">
           <Col className="about__col-1-1">
-
+            <h1>Stay updated, <br />
+                Never miss a post.
+            </h1>
           </Col>
 
           <Col className="about__col-1-2">
