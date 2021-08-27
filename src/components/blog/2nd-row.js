b73dbb55a6c58840c57d7464449b1c22c99e9819
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-08-27 22:47:05 +0700

    add 2nd row

---
+++ src/components/blog/2nd-row.js
@@ -0,0 +1,7 @@
+import Row from "../row"
+
+export default function SecondRow () {
+  return (
+    <Row className="blog__2nd-row"></Row>
+  )
+}
--- src/components/blog/blog.js
+++ src/components/blog/blog.js
@@ -1,9 +1,11 @@
 import FirstRow from "./1st-row"
+import SecondRow from "./2nd-row"
 
 export default function Blog () {
   return (
     <div className="blog">
       <FirstRow />
+      <SecondRow />
     </div>
   )
 }
