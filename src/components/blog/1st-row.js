96ea0ef19050ae22e2fbcc017ee9abe65df435cb
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-08-27 22:20:04 +0700

    .

---
+++ src/components/blog/1st-row.js
@@ -0,0 +1,7 @@
+import Row from "../row"
+
+export default function FirstRow () {
+  return (
+    <Row className="blog__1st-row"></Row>
+  )
+}
--- src/components/blog/blog.js
+++ src/components/blog/blog.js
@@ -1,7 +1,9 @@
-
+import FirstRow from "./1st-row"
 
 export default function Blog () {
   return (
-    <div className="blog"></div>
+    <div className="blog">
+      <FirstRow />
+    </div>
   )
 }
