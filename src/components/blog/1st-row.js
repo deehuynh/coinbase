fa9e3f743604a9cb2ace83918ef40ae7431dc4df
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-08-27 22:34:04 +0700

    .

--- src/components/blog/1st-row.js
+++ src/components/blog/1st-row.js
@@ -7,10 +7,12 @@ export default function FirstRow () {
   return (
     <Row className="blog__1st-row">
       <Col className="blog__col-1-1">
-        <h2>
+        <h5>HOME/BLOG</h5>
+
+        <h1>
           Stay updated, <br />
           Never miss a post.
-        </h2>
+        </h1>
 
         <p>
           Get to know the latest stuff about our services <br />
