6a34c78319732ceaf7077896e61a6e57f4907bd1
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-08-27 22:29:36 +0700

    .

--- src/components/blog/1st-row.js
+++ src/components/blog/1st-row.js
@@ -1,8 +1,18 @@
 import Row from "../row"
 import Col from "../column"
+import ColImage from "../column-image"
+import IMG1 from '../../images/blog-img-1.png'
 
 export default function FirstRow () {
   return (
-    <Row className="blog__1st-row"></Row>
+    <Row className="blog__1st-row">
+      <Col className="blog__col-1-1">
+        <h2>
+          Stay updated, <br />
+          Never miss a post.
+        </h2>
+      </Col>
+      <ColImage className="blog__col-1-2" image={IMG1} />
+    </Row>
   )
 }
