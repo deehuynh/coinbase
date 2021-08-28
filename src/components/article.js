ec8fecc465fc18dd5ffcd094df0ef96ecd0ecb82
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-08-29 05:06:17 +0700

    .

--- src/components/article.js
+++ src/components/article.js
@@ -5,7 +5,7 @@ export default function Article (props) {
     <article className={`article ` + props.className}>
       <img src={props.image} alt="Article img" />
       <h1>{props.title}</h1>
-      <h5><span>{props.time}</span> {props.genre}</h5>
+      <h5><span>{props.time}</span>{props.genre}</h5>
       <p>{props.description}</p>
       <Button className="button--linear-white button--pd-18-40" name="discover" />
     </article>
--- src/components/blog/2nd-row.js
+++ src/components/blog/2nd-row.js
@@ -1,11 +1,20 @@
 import Row from "../row"
 import Article from "../article"
+// Import image
+import Thumb1 from '../../images/blog-thumb-1.png'
 
 export default function SecondRow () {
   return (
     <Row className="blog__2nd-row">
       <Article 
-        
+        image={Thumb1}
+        title="HOW TO SELL YOUR ITUNES GIFTCARD WITH US"
+        time="19 JUNE, 2019" genre="business"
+        description="
+        Lorem ipsum dolor sit amet, magna habemus ius ad,
+        qui minimum voluptaria in. Ad mei modus quodsi complectitur, 
+        postea verterem persecuti cu est, sea epicuri.
+        "
       />
     </Row>
   )
