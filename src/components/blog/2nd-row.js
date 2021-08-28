238f1c5a6e874f14da48e55468faef16f8f7b337
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-08-29 05:42:21 +0700

    .

--- src/components/blog/2nd-row.js
+++ src/components/blog/2nd-row.js
@@ -6,7 +6,8 @@ import Thumb1 from '../../images/blog-thumb-1.png'
 export default function SecondRow () {
   return (
     <Row className="blog__2nd-row">
-      <Article 
+      <Article
+        className="article--mgbt-50"
         image={Thumb1}
         title="HOW TO SELL YOUR ITUNES GIFTCARD WITH US"
         time="19 JUNE, 2019" genre="business"
