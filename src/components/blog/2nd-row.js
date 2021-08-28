104d4004b49500e968965541c08e373d98c9ab61
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-08-28 22:26:18 +0700

    .

--- src/components/blog/2nd-row.js
+++ src/components/blog/2nd-row.js
@@ -4,7 +4,9 @@ import Article from "../article"
 export default function SecondRow () {
   return (
     <Row className="blog__2nd-row">
-
+      <Article>
+        
+      </Article>
     </Row>
   )
 }
