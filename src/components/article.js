ab99e8a6da3f1329fe693afe23e62d14c603ceaf
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-08-29 04:40:36 +0700

    .

--- src/components/article.js
+++ src/components/article.js
@@ -7,7 +7,7 @@ export default function Article (props) {
       <h1>{props.title}</h1>
       <h5><span>{props.time}</span> {props.genre}</h5>
       <p>{props.description}</p>
-      <Button className="button--linear-white" name="discover" />
+      <Button className="button--linear-white button--pd-18-40" name="discover" />
     </article>
   )
 }
