eff85c1d469faecf288454fcd05b1652d0935f2c
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-08-30 21:33:48 +0700

    .

--- src/components/contact/1st-row.js
+++ src/components/contact/1st-row.js
@@ -5,7 +5,19 @@ import ColImage from "../column-image"
 export default function FirstRow () {
   return (
     <Row className="contact__1st-row">
-      <Col className="contact__col-1-1"></Col>
+      <Col className="contact__col-1-1">
+        <h5>HOME/Contact us</h5>
+
+        <h1>
+          24/7 Customer support <br />
+          Including weekends.
+        </h1>
+
+        <p>
+          We’re always there ti help regardless the number <br />
+          of queries at hand.
+        </p>
+      </Col>
       <ColImage className="contact__col-1-2" />
     </Row>
   )
