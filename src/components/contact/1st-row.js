c5fa3edbaa5fd9f142544a9d2ff3a8eca81d6da5
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-08-30 21:31:21 +0700

    .

--- src/components/contact/1st-row.js
+++ src/components/contact/1st-row.js
@@ -1,7 +1,12 @@
-import Row from "../row";
+import Row from "../row"
+import Col from "../column"
+import ColImage from "../column-image"
 
 export default function FirstRow () {
   return (
-    <Row className="contact__1st-row"></Row>
+    <Row className="contact__1st-row">
+      <Col className="contact__col-1-1"></Col>
+      <ColImage className="contact__col-1-2" />
+    </Row>
   )
 }
