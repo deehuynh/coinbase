015212537cb547ccb962872ce0c2097d316b0818
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-08-31 23:04:48 +0700

    .

--- src/components/message-form.js
+++ src/components/message-form.js
@@ -3,7 +3,10 @@ import Button from './button'
 export default function MessageForm (props) {
   return (
     <form className="massage-form">
-      <div className="message-form__input-group"></div>
+      <div className="message-form__input-group">
+        <input type="text" />
+        <input type="text" />
+      </div>
     </form>
   )
 }
