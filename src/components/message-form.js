0be6338d842716018ac799ab1dc2e1b5d8b45ff0
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-08-31 23:08:10 +0700

    .

--- src/components/message-form.js
+++ src/components/message-form.js
@@ -7,6 +7,10 @@ export default function MessageForm (props) {
         <input type="text" />
         <input type="text" />
       </div>
+
+      <textarea name="" id="" cols="30" rows="10"></textarea>
+
+      <Button className="button--linear-white button--pd-15-40" name="publish" />
     </form>
   )
 }
