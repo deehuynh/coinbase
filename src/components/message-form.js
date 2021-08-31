f2b361a08f0ececc70f225c802c932294bddf409
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-08-31 23:09:25 +0700

    .

--- src/components/message-form.js
+++ src/components/message-form.js
@@ -4,11 +4,11 @@ export default function MessageForm (props) {
   return (
     <form className="massage-form">
       <div className="message-form__input-group">
-        <input type="text" />
-        <input type="text" />
+        <input type="text" placeholder="Name *" />
+        <input type="text" placeholder="Email *" />
       </div>
 
-      <textarea name="" id="" cols="30" rows="10"></textarea>
+      <textarea name="" id="" cols="30" rows="10" placeholder="Your comment"></textarea>
 
       <Button className="button--linear-white button--pd-15-40" name="publish" />
     </form>
