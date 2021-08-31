eefe83e60944aa33bc223f587d28c3857ff9bb12
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-08-31 22:59:44 +0700

    .

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,10 +1,12 @@
 import FirstRow from "./1st-row"
+import SecondRow from "./2nd-row"
 import ThirdRow from "./3th-row"
 
 export default function Contact (props) {
   return (
     <div className="contact">
       <FirstRow />
+      <SecondRow />
       <ThirdRow />
     </div>
   )
