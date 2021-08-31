48eecbe29d2bbc63d27b7afd1cefe4fe7e06225b
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-08-31 23:01:20 +0700

    .

--- src/components/contact/2nd-row.js
+++ src/components/contact/2nd-row.js
@@ -1,7 +1,10 @@
 import Row from "../row"
+import MessForm from '../message-form'
 
 export default function SecondRow () {
   return (
-    <Row className="contact__2nd-row"></Row>
+    <Row className="contact__2nd-row">
+      <MessForm />
+    </Row>
   )
 }
