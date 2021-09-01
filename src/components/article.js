fa5e2e61ff16b50b53772003decd275e051adb13
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-09-01 22:45:17 +0700

    modified article cpn

--- src/components/article.js
+++ src/components/article.js
@@ -4,10 +4,12 @@ export default function Article (props) {
   return (
     <article className={`article ` + props.className}>
       <img src={props.image} alt="Article img" />
-      <h1>{props.title}</h1>
-      <h5><span>{props.time}</span>{props.genre}</h5>
-      <p>{props.description}</p>
-      <Button className="button--linear-white button--pd-18-40" name="discover" />
+      <div className="article__info">
+        <h1>{props.title}</h1>
+        <h5><span>{props.time}</span>{props.genre}</h5>
+        <p>{props.description}</p>
+        <Button className="button--linear-white button--pd-18-40" name="discover" />
+      </div>
     </article>
   )
 }
