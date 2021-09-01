9ae23b7c9bd31e965a278a8fa87e04ce3417e1d3
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-09-01 22:36:40 +0700

    .

--- src/components/contact/2nd-row.js
+++ src/components/contact/2nd-row.js
@@ -4,6 +4,7 @@ import MessForm from '../message-form'
 export default function SecondRow () {
   return (
     <Row className="contact__2nd-row">
+      <p>Please note that all message will be replied within the next 12hours</p>
       <MessForm />
     </Row>
   )
