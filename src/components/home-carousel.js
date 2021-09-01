ece91f01af4c87dc15eed14f52e0e4d3695a8764
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-09-02 02:52:59 +0700

    .

--- src/components/home-carousel.js
+++ src/components/home-carousel.js
@@ -7,7 +7,37 @@ import Button from './button';
 export default function Carousel () {
   return (
     <div className="home-carousel">
-      <Splide>
+      <Splide
+        options = {{
+          type: 'loop',
+          perPage: 3,
+          perMove: 1,
+        }}
+      >
+        <SplideSlide>
+          <h5><span>19 JUNE, 2019</span> BUSINESS</h5>
+          <h2>ETH TO NAIRA EXCHANGE <br />
+            RATE IS NOW #400 PER...
+          </h2>
+          <p>Lorem ipsum dolor sit amet, magna habemus
+            ius ad, qui minimum voluptaria in. Ad mei modus
+            quodsi complectitur, postea...
+          </p>
+          <Button className='button--not-active button--pd-15-30' name='read more' />
+        </SplideSlide>
+
+        <SplideSlide>
+          <h5><span>19 JUNE, 2019</span> BUSINESS</h5>
+          <h2>ETH TO NAIRA EXCHANGE <br />
+            RATE IS NOW #400 PER...
+          </h2>
+          <p>Lorem ipsum dolor sit amet, magna habemus
+            ius ad, qui minimum voluptaria in. Ad mei modus
+            quodsi complectitur, postea...
+          </p>
+          <Button className='button--not-active button--pd-15-30' name='read more' />
+        </SplideSlide>
+
         <SplideSlide>
           <h5><span>19 JUNE, 2019</span> BUSINESS</h5>
           <h2>ETH TO NAIRA EXCHANGE <br />
