eeb5c47594e33cda3c584b55c21092426177b331
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-09-04 04:45:32 +0700

    modified about & blog component

--- src/components/about.js
+++ src/components/about.js
@@ -15,11 +15,26 @@ import SVG1 from '../svgs/Quality.svg'
 import SVG2 from '../svgs/medal.svg'
 import SVG3 from '../svgs/customer-review.svg'
 import SVG4 from '../svgs/support.svg'
+// Import hero
+import Hero from "./hero"
 
-export default function About (props) {
+export default function About () {
     return (
       <div className="about">
-        <Row className="about__1st-row">
+        <Hero className="hero__about" image={IMG1}>
+          <h5>HOME/about us</h5>
+
+          <h1>
+            Great service, <br />
+            Professional support
+          </h1>
+
+          <p>
+            CoinBase is a platform for trading your bitcoin and <br />
+            giftcard at the best rate, why not give us a trial.
+          </p>
+        </Hero>
+        {/* <Row className="about__1st-row">
           <Col className="about__col-1-1">
             <h5>HOME/about us</h5>
 
@@ -35,7 +50,7 @@ export default function About (props) {
           </Col>
 
           <ColImg className="about__col-1-2" image={IMG1} />
-        </Row>
+        </Row> */}
 
         <Row className="about__2nd-row">
           <Col className="about__col-2">
--- src/components/blog/1st-row.js
+++ src/components/blog/1st-row.js
@@ -2,8 +2,29 @@ import Row from "../row"
 import Col from "../column"
 import ColImage from "../column-image"
 import IMG1 from '../../images/blog-img-1.png'
+import Hero from "../hero"
 
 export default function FirstRow () {
+  const checkHero = true;
+  
+  if (checkHero) {
+    return (
+      <Hero className="hero__blog" image={IMG1}>
+        <h5>HOME/BLOG</h5>
+
+        <h1>
+          Stay updated, <br />
+          Never miss a post.
+        </h1>
+
+        <p>
+          Get to know the latest stuff about our services <br />
+          via our blog post.
+        </p>
+      </Hero>
+    )
+  }
+
   return (
     <Row className="blog__1st-row">
       <Col className="blog__col-1-1">
