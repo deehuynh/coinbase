a726e86d78f1a85cc7b37c3d2707a00c8f465c7e
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-09-06 00:22:53 +0700

    responsive for about page

--- src/components/about.js
+++ src/components/about.js
@@ -34,29 +34,12 @@ export default function About () {
             giftcard at the best rate, why not give us a trial.
           </p>
         </Hero>
-        {/* <Row className="about__1st-row">
-          <Col className="about__col-1-1">
-            <h5>HOME/about us</h5>
-
-            <h1>
-              Great service, <br />
-              Professional support
-            </h1>
-
-            <p>
-              CoinBase is a platform for trading your bitcoin and <br />
-              giftcard at the best rate, why not give us a trial.
-            </p>
-          </Col>
-
-          <ColImg className="about__col-1-2" image={IMG1} />
-        </Row> */}
 
         <Row className="about__2nd-row">
           <Col className="about__col-2">
             <h2>
-              “Morbi sagittis ultricies ex, a tempus lorem suscipit non.
-              Donec semper leo ut lobortis condimentum. Orci varius natoque penatibus et
+              “Morbi sagittis ultricies ex, a tempus lorem suscipit <br /> non.
+              Donec semper leo ut lobortis condimentum. Orci <br /> varius natoque penatibus et
               magnis”
             </h2>
 
