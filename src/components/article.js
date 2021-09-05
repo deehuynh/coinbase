7ae8b15857ee729de6c60cb652d0f52fe141675e
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-09-05 09:08:23 +0700

    responsive article

--- src/components/article.js
+++ src/components/article.js
@@ -4,11 +4,13 @@ export default function Article (props) {
   return (
     <article className={`article ` + props.className}>
       <img src={props.image} alt="Article img" />
-      <div className="article__info">
-        <h1>{props.title}</h1>
-        <h5><span>{props.time}</span>{props.genre}</h5>
-        <p>{props.description}</p>
-        <Button className="button--linear-white button--pd-18-40" name="discover" />
+      <div className="article__border-gradient">
+        <div className="article__info">
+          <h1>{props.title}</h1>
+          <h5><span>{props.time}</span>{props.genre}</h5>
+          <p>{props.description}</p>
+          <Button id="article__btn" className="button--linear-white button--pd-18-40" name="discover" />
+        </div>
       </div>
     </article>
   )
