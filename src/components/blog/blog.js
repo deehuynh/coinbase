a8c80eafa4981dd37ba63da1a3c24a6e11096b59
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-09-05 23:25:13 +0700

    add blog pagination

--- src/components/blog/blog.js
+++ src/components/blog/blog.js
@@ -1,11 +1,13 @@
 import FirstRow from "./1st-row"
 import SecondRow from "./2nd-row"
+import Pagination from "./pagination"
 
 export default function Blog () {
   return (
     <div className="blog">
       <FirstRow />
       <SecondRow />
+      <Pagination />
     </div>
   )
 }
---
+++ src/components/blog/pagination.js
@@ -0,0 +1,13 @@
+
+
+export default function Pagination () {
+  return (
+    <div className="blog__pagination">
+      <span className="blog__pagination--active">01</span>
+      <span>02</span>
+      <span>03</span>
+      <span>...</span>
+      <span>Next</span>
+    </div>
+  )
+}
