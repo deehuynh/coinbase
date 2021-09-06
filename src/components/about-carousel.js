23dc30c29096e1e4866b0ec0668a5372016dcd7a
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-09-06 14:46:02 +0700

    .

--- src/components/about-carousel.js
+++ src/components/about-carousel.js
@@ -1,5 +1,6 @@
 import { Carousel } from 'react-materialize';
 import "materialize-css";
+import { Link } from 'react-router-dom';
 // import "materialize-css/dist/css/materialize.min.css"
 
 export default function AboutCarousel () {
@@ -7,15 +8,20 @@ export default function AboutCarousel () {
     <Carousel
       className="about-carousel"
       options={{
-        dist: -30,
-        duration: 200,
+        dist: -40,
+        duration: 100,
       }}
     >
-      <a href="#one!"><img src="https://lorempixel.com/250/250/nature/1" /></a>
-      <a href="#two!"><img src="https://lorempixel.com/250/250/nature/2" /></a>
-      <a href="#three!"><img src="https://lorempixel.com/250/250/nature/3" /></a>
-      <a href="#four!"><img src="https://lorempixel.com/250/250/nature/4" /></a>
-      <a href="#five!"><img src="https://lorempixel.com/250/250/nature/5" /></a>
+      <Link to='/'>
+        <div>
+          <h5>CEO</h5>
+          <p>Amanda Roth</p>
+        </div>
+      </Link>
+      <Link to='/'></Link>
+      <Link to='/'></Link>
+      <Link to='/'></Link>
+      <Link to='/'></Link>
     </Carousel>
   )
 }
