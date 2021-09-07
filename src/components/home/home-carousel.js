9984f20b42b1f717382eaae22347cc7d26895527
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-09-07 15:26:52 +0700

    .

--- src/components/home/home-carousel.js
+++ src/components/home/home-carousel.js
@@ -19,31 +19,31 @@ export default function HomeCarousel () {
             RATE IS NOW #400 PER...
           </h2>
           <p>Lorem ipsum dolor sit amet, magna habemus
-            ius ad, qui minimum voluptaria in. Ad mei modus
+            ius <br /> ad, qui minimum voluptaria in. Ad mei modus <br />
             quodsi complectitur, postea...
           </p>
           <Button className='button--not-active button--pd-15-30' name='read more' />
         </div>
 
         <div className="home-carousel__slide">
-          <h5><span>19 JUNE, 2019</span> BUSINESS</h5>
-          <h2>ETH TO NAIRA EXCHANGE <br />
-            RATE IS NOW #400 PER...
-          </h2>
+          <h5><span>19 JUNE, 2019</span> LIFESTYLE</h5>
+          <h2>EXCHANGE 5 PRODUCTS & <br />
+                      GET 1 FREE PRODUCT.
+                  </h2>
           <p>Lorem ipsum dolor sit amet, magna habemus
-            ius ad, qui minimum voluptaria in. Ad mei modus
+            ius <br /> ad, qui minimum voluptaria in. Ad mei modus <br />
             quodsi complectitur, postea...
           </p>
           <Button className='button--not-active button--pd-15-30' name='read more' />
         </div>
 
         <div className="home-carousel__slide">
-          <h5><span>19 JUNE, 2019</span> BUSINESS</h5>
-          <h2>ETH TO NAIRA EXCHANGE <br />
-            RATE IS NOW #400 PER...
-          </h2>
-          <p>Lorem ipsum dolor sit amet, magna habemus
-            ius ad, qui minimum voluptaria in. Ad mei modus
+          <h5><span>19 JUNE, 2019</span> TECHNOLOGY</h5>
+          <h2>HOW TO SELL YOUR ITUNES <br />
+                      GIFTCARD WITH US .
+                  </h2>
+                  <p>Lorem ipsum dolor sit amet, magna habemus
+            ius <br /> ad, qui minimum voluptaria in. Ad mei modus <br />
             quodsi complectitur, postea...
           </p>
           <Button className='button--not-active button--pd-15-30' name='read more' />
