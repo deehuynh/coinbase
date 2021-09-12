c5aa0a13a3fb93d67f285c3e5888e1ae65f31c51
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-09-12 10:00:57 +0700

    .

---
+++ src/components/top-btn.js
@@ -0,0 +1,9 @@
+import TopArrow from "../svgs/top-arrow.svg"
+
+export default function ToTopBtn () {
+  return (
+    <div className="scroll-to-top">
+      <img src={TopArrow} alt="Top Arrow" />
+    </div>
+  )
+}
