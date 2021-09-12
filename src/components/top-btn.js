cd5ca6f6d77b9ef93ea74431b85c1db12d6457bb
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-09-12 10:31:02 +0700

    .

--- src/components/top-btn.js
+++ src/components/top-btn.js
@@ -1,8 +1,24 @@
 import TopArrow from "../svgs/top-arrow.svg"
+import { useRef, useEffect } from "react";
 
 export default function ToTopBtn () {
+  const ScrollToTop = () => {
+    window.scrollTo(0,0);
+  }
+
+  const refContainer = useRef(null);
+  useEffect(()=>{
+    window.onscroll = () => {
+      if (window.pageYOffset > 1000) {
+        refContainer.current.style = "display: block";
+      } else {
+        refContainer.current.style = "display: none";
+      }
+    }
+  });
+
   return (
-    <div className="scroll-to-top">
+    <div ref={refContainer} style={{display: 'none'}} onClick={ScrollToTop} className="scroll-to-top">
       <img src={TopArrow} alt="Top Arrow" />
     </div>
   )
