fd5fff9c464b62cd416ba32f359fb6ab39359751
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2022-03-11 11:02:49 +0700

    setup apollo client

--- src/index.js
+++ src/index.js
@@ -3,6 +3,32 @@ import ReactDOM from 'react-dom';
 import './styles/index.scss';
 import Layout from './components/Layout';
 import reportWebVitals from './reportWebVitals';
+// apollo client
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
+
+// const client = ...
+
+client.query({
+    query: gql`
+      query GetRates {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `
+  }).then(result => console.log(result));
+
 
 ReactDOM.render(
   <React.StrictMode>
