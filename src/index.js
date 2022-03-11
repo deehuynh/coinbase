2f7660b447e160d8a660859febfc632265b63df4
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2022-03-11 11:10:17 +0700

    modified_src/index.js_connect apollo_client_to_react_app

--- src/index.js
+++ src/index.js
@@ -29,10 +29,38 @@ client.query({
     `
   }).then(result => console.log(result));
 
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`;
+
+function ExchangeRates() {
+
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.rates.map(({ currency, rate }) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
+    </div>
+  ));
+}
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <Layout />
+    <ApolloProvider client={client}>
+      <ExchangeRates />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
